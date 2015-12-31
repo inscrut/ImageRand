dc1e6c1207edbc451ddac7387c9c625d978eaf25
Кирилл С. <kirr2@gf.tom.ru>
2015-12-31 13:18:47 +0600

    Release 1.0.0.0: First commit;

---
+++ ImageRand/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+using System.Security.Cryptography;
+
+namespace ImageRand
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter size (ex. 500x250): ");
+            string[] buf;
+            Size size = new Size();
+            try
+            {
+                buf = Console.ReadLine().Split('x');
+                size.Width = Convert.ToInt32(buf[0]);
+                size.Height = Convert.ToInt32(buf[1]);
+            }
+            catch
+            {
+                MessageBox.Show("ERR: Неверно введен размер изображения!\r\nИспользуйте формат (пример): 100x100");
+                Environment.Exit(1);
+            }
+
+            Bitmap img = new Bitmap(size.Width, size.Height);
+            //Random rnd = new Random();
+            for (int i = 0; i < size.Width; i++)
+            {
+                for (int j = 0; j < size.Height; j++)
+                {
+                    img.SetPixel(i, j, /*rnd.Next(0, 2) == 0 ? Color.Black : Color.Transparent*/ GenerateRandomNumber(0, 2) == 0 ? Color.Black : Color.Transparent);
+                }
+            }
+            img.Save("maked.png", System.Drawing.Imaging.ImageFormat.Png);
+
+            Console.WriteLine("Complete! Press any key . . .");
+            Console.Read();
+        }
+        static int GenerateRandomNumber(int min, int max)
+        {
+            RNGCryptoServiceProvider c = new RNGCryptoServiceProvider();
+            byte[] randomNumber = new byte[4]; // Integer 4 Byte
+            c.GetBytes(randomNumber);
+            int result = Math.Abs(BitConverter.ToInt32(randomNumber, 0));
+            
+            // Max. randomnumber Min. randomnumber
+            return result % max + min;
+        }
+    }
+}
---
+++ ImageRand/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Управление общими сведениями о сборке осуществляется с помощью 
+// набора атрибутов. Измените значения этих атрибутов, чтобы изменить сведения,
+// связанные со сборкой.
+[assembly: AssemblyTitle("ImageRand")]
+[assembly: AssemblyDescription("Создать ч/б изображение с псевдо-случайным рисунком")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Home")]
+[assembly: AssemblyProduct("ImageRand")]
+[assembly: AssemblyCopyright("Copyright © Home 2015")]
+[assembly: AssemblyTrademark("GF")]
+[assembly: AssemblyCulture("")]
+
+// Параметр ComVisible со значением FALSE делает типы в сборке невидимыми 
+// для COM-компонентов.  Если требуется обратиться к типу в этой сборке через 
+// COM, задайте атрибуту ComVisible значение TRUE для этого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("2c0446f3-e506-4d52-9ff3-5a388d39c004")]
+
+// Сведения о версии сборки состоят из следующих четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии 
+//   Номер сборки
+//      Редакция
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
